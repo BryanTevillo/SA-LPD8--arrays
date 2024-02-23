d9f92d9f40080a0c3cf042ea673370775db0f91c
BryanTevillo <tevillo24@gmail.com>
2024-02-23 14:43:19 -0600

    Se agrega cambios ejercicio se termina

--- src/com/generation/OrderIsPrime.java
+++ src/com/generation/OrderIsPrime.java
@@ -10,9 +10,7 @@ public class OrderIsPrime {
     int[] arrayOrder = new int[10];
 
     Scanner in = new Scanner(System.in);
-    System.out.println(
-      "Programa ordena array primos Favor de ingresar los siguientes 10 numeros "
-    );
+    System.out.println("Programa ordena array primos Favor de ingresar los siguientes 10 numeros ");
 
     for (int i = 0; i < 10; i++) {
       System.out.println("Ingresa un numero: ");
@@ -20,23 +18,18 @@ public class OrderIsPrime {
     }
     System.out.println("***************Array Original***************");
     printArrya(arrayMain);
-    int count = 0;
+    int countPrime = 0;
+    int countNonPrime = arrayMain.length - 1; // Empieza desde el final del arreglo
+
     for (int num : arrayMain) {
       if (isPrime(num)) {
-        arrayOrder[count] = num;
-        count++;
-      }
-    }
-    for (int num : arrayMain) {
-      if (!isPrime(num)) {
-        arrayOrder[count] = num;
-        count++;
+        arrayOrder[countPrime++] = num; // Añade al principio del arreglo de primos
+      } else {
+        arrayOrder[countNonPrime--] = num; // Añade al final del arreglo de no primos
       }
     }
 
-    System.out.println(
-      "***************Array Ordenado Primos Primero***************"
-    );
+    System.out.println("***************Array Ordenado Primos Primero***************");
     printArrya(arrayOrder);
 
     in.close();
